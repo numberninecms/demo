7da102c39d917babc883f6438786a559d2b38bac
William Arin <williamarin.dev@gmail.com>
2020-10-30 17:14:50 +0800

    build: containers are running but not accessible by http yet

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -3,26 +3,31 @@ set -e
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+    set -- php-fpm7 "$@"
 fi
 
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
-	if [ "$APP_ENV" != 'prod' ]; then
-		PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
-	fi
-	ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
+if [ "$1" = 'php-fpm7' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
+    PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-production"
+    if [ "$APP_ENV" != 'prod' ]; then
+        PHP_INI_RECOMMENDED="$PHP_INI_DIR/php.ini-development"
+    fi
+    ln -sf "$PHP_INI_RECOMMENDED" "$PHP_INI_DIR/php.ini"
 
-  mkdir -p var/cache var/log
+    mkdir -p var/cache var/log
 
-  if [ "$APP_ENV" != 'prod' ]; then
-      #composer install --prefer-dist --no-progress --no-suggest --no-interaction
-      echo 'DATABASE_URL=mysql://user:user@localhost:3306/numbernine_app?serverVersion=5.7' >> .env.local
-      make install
-  fi
+    if [ ! -f .env.local ]; then
+        echo 'DATABASE_URL=mysql://user:user@mysql:3306/numbernine_app?serverVersion=5.7' >.env.local
+        make install
+        jq '.extra.symfony.docker=true' tmp/composer.json >tmp/composer.tmp.json
+        rm tmp/composer.json
+        mv tmp/composer.tmp.json tmp/composer.json
 
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+        cp -Rp tmp/. .
+        rm -Rf tmp/
+    fi
+
+    setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+    setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 fi
 
 exec docker-php-entrypoint "$@"
